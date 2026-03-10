1afc3266572a6d52c28acb92a6630dee21c8b761
Saketaram Manikantan <79354282+SakMan27@users.noreply.github.com>
2026-03-10 11:00:04 +0530

    Add files via upload

---
+++ (skip) pygame platformer starting.py
@@ -0,0 +1,20 @@
+import pygame
+import random
+from settings import *
+
+
+
+all_sprites = pygame.sprite.Group()
+# Game loop
+running = True
+while running:
+    # keep loop running at the right speed
+    # Process input (events)
+    
+    # Update
+
+    # Draw / render
+
+
+pygame.quit()
+
